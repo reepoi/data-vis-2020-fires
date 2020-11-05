ff68a38333913d93979c5c48d3a64451560a8cda
Huy Tran <baohuy251210@gmail.com>
2020-11-04 17:32:22 -0700

    Hash paging added, Working on buttons

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/* Other DOM Manipulation before Data load: */
+window.addEventListener('load', function() {
+    console.log('All assets are loaded');
+    pagingHandler();
+});
+window.addEventListener("hashchange", function(e) {
+    pagingHandler();
+});
+
+/* Visualization: After data load */
+loadData().then(data => {
+    console.log(data);
+
+    /* TODO: D3 Visualizations here */
+
+
+
+    /*Set up website handlers (internal) after data load*/
+    /* Update Visualizations */
+
+})
+
+/* Async function to load files that you want */
+async function loadData() {
+    let perimeters = await d3.json("./data/WF_Perimeters.geojson");
+    let points = await d3.json("./data/WF_Points.geojson");
+    return {
+        'perimeters': perimeters,
+        'points': points,
+    };
+}
+
+
+
+
+/* Views Helper Functions */
+/**
+ * This function handles the website paging
+ * with hash locations.
+ */
+function pagingHandler() {
+    let currentHash = window.location.hash;
+    switch (currentHash) {
+        case "#compare-year":
+            /* This is just a demo function I made to see how tabbing works */
+            /* TODO: Add function to handle this */
+            break;
+        case "#stories":
+            /* This is just a demo function I made to see how tabbing works */
+            /* TODO: Add stories function*/
+            break;
+        case "#fire-map":
+        default:
+            /* TODO: Add fire map function */
+            currentHash = "#fire-map";
+            break;
+
+    }
+    window.location.hash = currentHash;
+    /* Update Views (buttons, tabs, enable, selection...) */
+    activeTabHandler(currentHash);
+}
+
+
+/**
+ * This function changes the DOM classes of the tabs 
+ * corresponding to the user's selection (as shown in the page hashtag)
+ * This doesn't touch any Visualizations or Data content
+ */
+function activeTabHandler(currentHash) {
+    let tabHashes = [];
+    let tabsSelection = d3.select("#menu").selectAll(".tab-item")
+        .each(function() {
+            let aElement = d3.select(this).select("a");
+            if (aElement.attr("href") === currentHash) {
+                console.log(aElement);
+                aElement.classed("active", true)
+            } else
+                aElement.classed("active", false)
+            tabHashes.push(aElement.attr("href"));
+        });
+    //Disable and Enable at left and right most tabs:
+    //if hashtag = left most, then disable back button
+    d3.select("#menu-back").classed("disabled", (_) => currentHash === tabHashes[0]);
+    //if hashtag = right most, then disable forward button
+    d3.select("#menu-forward").classed("disabled", (_) => currentHash === tabHashes[tabHashes.length - 1]);
+
+}
