9b4dac2aef1ff44316c1202b1bfdf6962618e33a
Taos Transue <spacechips@rocketmail.com>
2020-11-22 01:03:29 -0700

    Now can select fires on map through bar chart

--- js/script.js
+++ js/script.js
@@ -33,15 +33,18 @@ function fireMapInitialize() {
         mapView.drawMapFeatures(data, fireInfo.currentPage);
 
         /**TODO: Linking Functions go HERE: */
+        function updateMapView(selectedFire){
+            mapView.selectAndZoomToPolygon(selectedFire);
+        }
         /**
          * 
          * @param {leaflet e.target} selectedFire - Leaflet e.target
          */
         function updateFireInfo(selectedFire) {
             fireInfo.updateSelectedFireInfo(selectedFire);
-
         }
         mapView.updateFireInfo = updateFireInfo;
+        fireInfo.updateMapView = updateMapView;
         fireInfo.updateFireInfo = updateFireInfo;
 
         /*
