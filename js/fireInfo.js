a63d5daa0d6e308f87f0bd674b84bf9e8e2cf1c4
Huy Tran <baohuy251210@gmail.com>
2020-12-02 11:03:11 -0700

    updated panel info

--- js/fireInfo.js
+++ js/fireInfo.js
@@ -616,11 +616,11 @@ class FireInfo {
         switch (statName) {
             case "SizeArea":
             default:
-                return ["Area Burned:", `${this.numberWithCommas(this.totalAreaBurned)} acres`, 'Largest Fires by Area:'];
+                return ["Total Area Burned (.est)", `${this.numberWithCommas(this.totalAreaBurned)} acres`, 'Largest Fires by Area:'];
             case "StructuresDestroyed":
-                return ["Structures Destroyed:", `${this.numberWithCommas(this.totalStructuresDestroyed)}`, 'Most Structures Destroyed:'];
+                return ["Total Structures Destroyed (.est)", `${this.numberWithCommas(this.totalStructuresDestroyed)}`, 'Most Structures Destroyed:'];
             case "SuppresionCost":
-                return ["Suppresion Cost:", `${this.numberWithCommas(this.totalSuppressionCost)}`, 'Highest Suppression Cost:'];
+                return ["Total Suppresion Cost (.est)", `$${this.numberWithCommas(this.totalSuppressionCost)}`, 'Highest Suppression Cost:'];
 
         }
     }
